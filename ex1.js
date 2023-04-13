931202cc919eff470b3ee07cd84930941980ec22
negararna <negararna90@gmail.com>
2023-04-13 17:20:08 +0430

    modified:   ex1.js

--- ex1.js
+++ ex1.js
@@ -3,6 +3,7 @@
 // Create a variable called x, assign the value 50 to it.
 // Answer:
 
+let x = 50;
 
 
 
@@ -23,10 +24,10 @@
 // Exercise 1.3 - Data Types
 // Describe the correct data type of the following variables:
 
-//let width = 30              //Answer:
-//let height = 40.2           //Answer:
-//let title = "Hello"         //Answer:
-//const person = {            //Answer:
+//let width = 30              //Answer: the width variable is a number type
+//let height = 40.2           //Answer: the height variable is a number type
+//let title = "Hello"         //Answer: the titele variable is a stinge type
+//const person = {            //Answer:  the person variabel constant is also an object type because it is defined as a group
  //   firstName: "Ali",
  //   lastName: "Akbari"
 //}
